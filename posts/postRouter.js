79d83760088acb0675a95f3d0845c117e2e0ece2
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-30 15:28:48 -0500

    validatePostId created

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,28 +1,32 @@
 /*dependencies & inports*/
-const express = require('express');
-
+const express = require("express");
+const PostsDB = require("./postDb");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+/*requests*/
 
-});
+router.get("/", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+  const id = req.params.id;
+  PostsDB.getById(id)
+    .then(posts => {
+      !posts
+        ? res.status(404).json({ message: "invalid user id" })
+        : (req.posts = posts);
+      next();
+    })
+    .catch(err => {
+      res.status(400).json({ err });
+    });
+}
 
 module.exports = router;
